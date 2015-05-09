8b88f7f3cf34a8b1b82b57a17c07a9e33210a958
Andrew Meyer <andrewm1100@gmail.com>
2015-05-09 11:56:09 -0600

    Updated Gaussian plot example to rotate points continuously

--- Projection/examples/GaussianPlot/GaussianPlot.ino
+++ Projection/examples/GaussianPlot/GaussianPlot.ino
@@ -11,8 +11,13 @@
 
 #define UPDATE_DELAY  0
 
-// Transform for plot (note Z scale)
-Transform plotTrans = Transform(0, 0, 45, 1, 1, 3, 0, 0, 0);
+// Transform for plot
+Transform plotTrans;
+float angle = 0;
+
+// Array of plot points
+point3 **points;
+unsigned int numPtsX, numPtsY;
 
 // Create a camera
 Camera cam(LCDWIDTH, LCDHEIGHT);
@@ -33,49 +38,56 @@ void setup()
     // Uncomment these two lines for orthographic projection
     //cam.projMode = PROJ_ORTHO;
     //cam.orthoViewWidth = 3.0;
+    
+    // Compute the points in the Gaussian plot
+    computePoints();
 }
 
 void loop()
 {
     uView.clear(PAGE);
     
-    // Draw Gaussian plot
-    drawGaussPlot();
+    // Update plot transform and draw points
+    plotTrans = Transform(0, 0, angle += 2, 1, 1, 3, 0, 0, 0);
+    drawPoints();
     
     uView.display();
     delay(UPDATE_DELAY);
 }
 
-void drawGaussPlot()
+void computePoints()
 {
     float ux = 0, uy = 0;      // Means
-    float sx = 0.5, sy = 0.5;  // Standard deviations
+    float sx = 0.5, sy = 0.7;  // Standard deviations
     
     // Plotting options
     float minX = -3, maxX = 3;
     float minY = -3, maxY = 3;
-    float xStep = 0.25, yStep = 0.25;
+    float xStep = 0.75, yStep = 0.75;
     
-    byte numPtsX = (maxX - minX) / xStep;
-    byte numPtsY = (maxY - minY) / yStep;
+    numPtsX = (maxX - minX) / xStep;
+    numPtsY = (maxY - minY) / yStep;
     
-    for (byte n = 0; n < numPtsX; n++)
+    points = new point3*[numPtsX];
+    
+    if (points != NULL)
     {
-        for (byte m = 0; m < numPtsY; m++)
+        for (unsigned int n = 0; n < numPtsX; n++)
         {
-            // Compute Gaussian point
-            float x = (n * xStep) + minX;
-            float y = (m * yStep) + minY;
-            float z = gauss(x, y, ux, uy, sx, sy);
-            point3 pt3 = { x, y, z };
+            // Try to allocate next row of points
+            points[n] = new point3[numPtsY];
             
-            // Transform and project 3D point
-            point2 pt2 = cam.project(plotTrans * pt3);
-            
-            // Draw point if not clipped
-            if (!isnan(pt2.x))
+            if (points[n] != NULL)
             {
-                uView.pixel(pt2.x, pt2.y);
+                for (unsigned int m = 0; m < numPtsY; m++)
+                {
+                    // Compute Gaussian point
+                    float x = (n * xStep) + minX;
+                    float y = (m * yStep) + minY;
+                    float z = gauss(x, y, ux, uy, sx, sy);
+                    
+                    points[n][m] = { x, y, z };
+                }
             }
         }
     }
@@ -87,4 +99,32 @@ float gauss(float x, float y, float ux, float uy, float sx, float sy)
         ((x - ux) * (x - ux) / (2 * sx * sx)) +
         ((y - uy) * (y - uy) / (2 * sy * sy))))
         / (2 * PI * sx * sy);
+}
+
+void drawPoints()
+{
+    for (unsigned int n = 0; n < numPtsX; n++)
+    {
+        // Check for low memory
+        if (points == NULL || points[n] == NULL)
+        {
+            uView.clear(PAGE);
+            uView.setCursor(0, 0);
+            uView.print("Out of\nmemory!\n\nIncrease\nX, Y step\nsize");
+        }
+        else
+        {
+            for (unsigned int m = 0; m < numPtsY; m++)
+            {
+                // Transform and project 3D point
+                point2 pt = cam.project(plotTrans * points[n][m]);
+                    
+                // Draw point if not clipped
+                if (!isnan(pt.x))
+                {
+                    uView.pixel(pt.x, pt.y);
+                }
+            }
+        }
+    }
 }
